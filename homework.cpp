152df948d0fbef76f4e90fdca6d6375e68d588e2
Apham130 <andypham4747@gmail.com>
2025-01-14 21:49:16 +0000

    hw pt 1 done

--- homework.cpp
+++ homework.cpp
@@ -11,10 +11,34 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
+
+int input1;
+int input2;
+
+
+void compare(double x, double y)
+{
+    if (x>y)
+    {
+        std::cout << "The first input is larger than the second input.\n";
+    } else if (x<y)
+    {
+        std::cout << "The first input is smaller than the second input.\n";
+    } else if (x==y)
+    {
+        std::cout << "Both inputs are equal.\n";
+    }
+}
+
+
 int main()
 {
-    // Complete your homework here
 
+
+    // Complete your homework here
+    std::cout << "Enter 2 numbers:\n";
+    std::cin >> input1 >> input2;
+    compare(input1,input2);
     
     return 0;
 }
